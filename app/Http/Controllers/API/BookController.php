4c57647b80541fd89ff9e066dd688ebdf9290813
Chavalit Koweerawong <chavalit.kow@gmail.com>
2022-06-05 14:01:52 +0700

    Week13

---
+++ app/Http/Controllers/API/BookController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Book::get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $requestData = $request->all();
+        if ($request->hasFile('image')) {
+            $filename = $request->file('image')->store('uploads', 'public');
+            $requestData['image'] = asset('storage/' . $filename);
+        }
+        $item = Book::create($requestData);
+        return ["success" => true, "data" => $item];
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Book::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $requestData = $request->all();
+        if ($request->hasFile('image')) {
+            $filename = $request->file('image')->store('uploads', 'public');
+            $requestData['image'] = asset('storage/' . $filename);
+        }
+        $item = Book::findOrFail($id);
+        $success = $item->update($requestData);
+        return ["success" => $success];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = Book::findOrFail($id);
+        $success = $item->delete();
+        return ["success" => $success];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\BookController;
+use App\Http\Controllers\API\LocationController;
 use App\Http\Controllers\API\TambonController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +26,11 @@ Route::get('/provinces', [ TambonController::class , 'getProvinces' ]);
 Route::get('/amphoes', [TambonController::class , 'getAmphoes' ]);
 Route::get('/tambons', [ TambonController::class , 'getTambons' ]);
 Route::get('/zipcodes', [TambonController::class, 'getZipcodes'] );
+
+Route::apiResource('book', BookController::class);
+Route::apiResource('location', LocationController::class);
+
+
+Route::post('/sanctum/token', [UserController::class, 'token']);
+Route::post('/sanctum/token/register', [UserController::class, 'register']);
+
